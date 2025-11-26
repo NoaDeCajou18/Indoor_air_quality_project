76f01d92404b0de250b15362cb3ab20bf907e0c6
Becas Laura (276910) <276910@vutbr.cz>
2025-11-26 16:50:06 +0100

    update main

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-from machine import Timer
+from machine import Timer, I2C, Pin
 # wifi
 import network
 import wifi_utils
 import config # name and password of network
 import urequests  # Network Request Module
 # Sensors
-from dht12.py import DHT12
+from dht12 import DHT12
+from mq135 import MQ135
+# Display
+import i2c_display
 
 #TODO adapte period of timer and period of measure
 
 
 # constants
-PERIODE_MEASURES = 500
+PERIODE_MEASURES = 10
 
 # Varibales
 cnt = 0
@@ -27,17 +30,21 @@ def timer_handler(t):
     if cnt % PERIODE_MEASURES == 0 :
         readData = True
 
-tim.init(period=1, mode=Timer.PERIODIC, callback=timer_handler)     # period in ms 
+tim.init(period=100, mode=Timer.PERIODIC, callback=timer_handler)     # period in ms 
 
 # Sensors
 # Connect to the DHT12 sensor
 i2c = I2C(0, scl=Pin(22), sda=Pin(21), freq=400_000)
-sensor_dht12 = dht12.DHT12(i2c)
+sensor_dht12 = DHT12(i2c)
+MyMQ = MQ135(36)
 
+
+# Display
+i2c_display.init_oled()
 
 
 # Wifi
-API_KEY = "" #TODO
+API_KEY = "IHK5I6MLU9OLLS9V" 
 def send_to_thingspeak(temp, humidity,co2, pm):
     API_URL = "https://api.thingspeak.com/update"
 
@@ -55,17 +62,15 @@ wifi = network.WLAN(network.STA_IF)     # Initialize the Wi-Fi interface in Stat
 try:
     while True:
         if readData:
-            # TODO get data
-            temp, humidity = sensor_dth12.read_values()
-            co2, pm = getData()
-
+            temp, humidity = sensor_dht12.read_values()
+            co2 = MyMQ.getCorrectedPPM(temp, humidity)
+            pm  = 2 # TODO get data
+            
             wifi_utils.connect(wifi, config.SSID, config.PSWD)
             send_to_thingspeak(temp, humidity, co2, pm)
             wifi_utils.disconnect(wifi)
 
-            # TODO print data on OLED
-            print(data)
-
+            i2c_display.update_oled(temp, humidity, co2, pm)
             readData = False
         
 except KeyboardInterrupt:
